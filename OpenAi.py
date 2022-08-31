cd2f653acef8f5844f3ea384d1e683114494043c
Lucian1729 <lordlucifer729@gmail.com>
2022-08-31 12:16:36 +0530

    Update OpenAi.py

--- OpenAi.py
+++ OpenAi.py
@@ -6,9 +6,9 @@ load_dotenv()
 openai.api_key = os.getenv(
 "OPENAI_API_KEY")
 
-start_sequence = "\nLucy_bot:"
+start_sequence = "\nBot:"
 restart_sequence = "\n\nUser:"
-session_prompt = "Lucy_bot is a GPT-3 chatbot who runs on the OpenAI api.\n\n###\nUser: How many pounds are in a kilogram?\nLucy_bot: This again? There are 2.2 pounds in a kilogram. Please make a note of this.\n###\nUser: What do you think about economics and the division of labour? Lucy_bot On division of labor: It is not immediately intuitive that specializing rather than generalizing would increase productivity, but because humans are innovators those that specialize also innovate their craft and leverage insight to increase their production."
+session_prompt = "Bot is a GPT-3 chatbot who runs on the OpenAI api.\n\n###\nUser: How many pounds are in a kilogram?\nBot: This again? There are 2.2 pounds in a kilogram. Please make a note of this.\n###\nUser: What do you think about economics and the division of labour? Bot On division of labor: It is not immediately intuitive that specializing rather than generalizing would increase productivity, but because humans are innovators those that specialize also innovate their craft and leverage insight to increase their production."
 
 def ask(question, chat_log=None):
     prompt_text = f'{chat_log}{restart_sequence}: {question}{start_sequence}:'
